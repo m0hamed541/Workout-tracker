bd66bd2ced7b6cbe21f4ce335069750d54f6456e
Agougil Mohamed <agougilmohamed03@gmail.com>
2025-04-15 17:39:35 +0100

    home screen test

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  blockConatiner: {
+    backgroundColor: "#1c1c1c", // Dark background color
+    borderRadius: 16, // Rounded corners as seen in the image
+    padding: 20,
+    marginVertical: 8, // Vertical spacing between containers
+    marginHorizontal: 16,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+});
+
+export default styles;
